bbad8884264c8584d3742e0a50bbeff0d015877b
AlejandraKeber <alejandrakeber@gmail.com>
2023-02-22 12:28:10 -0500

    Insert data into species table

--- data.sql
+++ data.sql
@@ -38,3 +38,8 @@ INSERT INTO owners (full_name, age)
 VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age)
 VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon');
+INSERT INTO species (name)
+VALUES ('Digimon');
