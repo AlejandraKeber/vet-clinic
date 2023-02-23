6573c2d9909ab6101e9ae7f1ba02d7c2634414b5
AlejandraKeber <alejandrakeber@gmail.com>
2023-02-23 12:50:20 -0500

    Create vets, specializations and visits tables

--- schema.sql
+++ schema.sql
@@ -33,3 +33,25 @@ ALTER TABLE animals
 ADD COLUMN species_id INT CONSTRAINT species_fk REFERENCES species (id);
 ALTER TABLE animals
 ADD COLUMN owner_id INT CONSTRAINT owner_fk REFERENCES owners (id);
+
+-- Add join table for visits
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations(
+    species_id INT,
+    vet_id INT,
+    PRIMARY KEY(species_id, vet_id)
+);
+
+CREATE TABLE visits(
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    PRIMARY KEY(animal_id, vet_id, visit_date)
+);
